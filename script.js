0e597d53b424efb61475407eb9a8015adb0f8add
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2022-08-29 21:26:57 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+var logoElement = $('footer .hide');
+
+$(window).scroll(function() {
+  
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+     
+       $(logoElement).addClass('show');
+     
+   } else if($(logoElement).hasClass('show') && $(window).scrollTop() + $(window).height() > $(document).height() - 150) {
+     
+      $(logoElement).removeClass('show');
+     
+   }
+});
+
+});
+
